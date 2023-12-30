4ffc2072ed3c85c413d13ee3539bf753cf750775
lazyPy <janopatundog@gmail.com>
2023-12-30 13:39:02 +0800

    first commit

--- Abbey_Monastry/settings.py
+++ Abbey_Monastry/settings.py
@@ -96,8 +96,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
