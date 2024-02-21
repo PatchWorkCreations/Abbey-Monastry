3b98f8c1144db562fa3314585c33112e858b2b96
lazyPy <janopatundog@gmail.com>
2024-02-21 09:06:36 +0800

    .

--- Src_App/models.py
+++ Src_App/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Prayer(models.Model):
     name = models.CharField(max_length=200)
     type = models.CharField(max_length=200)
-    prayer_msg = models.CharField(max_length=200)
+    prayer_msg = models.CharField(max_length=2000)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
